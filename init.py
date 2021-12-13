1d2dcf0ad05483c295b91ac47e535d04e1eaa7ce
kimdonghyun <snu2589@gmail.com>
2021-12-13 20:51:21 +0900

    11

--- init.py
+++ init.py
@@ -1,2 +1,12 @@
 import sqlite3
-sqlite3.connect(data.db')
+sqlite3.connect('data.db')
+import sqlite3
+con1 = sqlite3.connect('data.db')
+cursor = con1.cursor()
+cursor.execute('''
+    CREATE TABLE topics (
+        id        INTEGER      PRIMARY KEY AUTOINCREMENT,
+        title     VARCHAR (50) NOT NULL,
+        body      TEXT
+    )
+''')
