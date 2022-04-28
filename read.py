c0df3e2a5074acc2fbfd39eae5d095c78cf43e52
kimdonghyun77 <snu2589@gmail.com>
2022-04-28 16:54:07 +0900

    se commit

--- read.py
+++ read.py
@@ -1,18 +1,59 @@
 import sqlite3
-# connect = sqlite3.connec
-with cursor = connect.curs
+# connect = sqlite3.connecimport sqlite3
+# connect = sqlite3.connect('data.db')
+with sqlite3.connect('data.db') as connect;
+cursor = connect.cursor()
 
-title = input('제목을 알려
-body = input('본문을 알려
-sql = 'insert into topics 
-cursor.execute(sql, (title
+sql = ' select id,title,body from topics'
 
-# sql = 'insert into topic
+#title = input('제목을 알려주세요: ')
+#body = input('본문을 알려주세요: ')
+#sql = 'insert into topics (title, body) values(?,?)'
 
 cursor.execute(sql)
+row = cursor.fetchall()
+print(rows)
 
-connect.commit()
-cursor.close()
-connect.close()
+#row = cursor.fetchone()
+# sql = 'insert into topics (title, body) values("'+title+'","'+body+'")'sql인젝션문제 
+#connect.commit()
+#cursor.close()
+#connect.close()
 
 # print(title, body)  
+#import sqlite3
+# connect = sqlite3.connect('data.db')
+# with cursor = connect.cursor()
+
+#title = input('제목을 알려주세요: ')
+#body = input('본문을 알려주세요: ')
+#sql = 'insert into topics (title, body) values(?,?)'
+#cursor.execute(sql, (title, body))
+
+# sql = 'insert into topics (title, body) values("'+title+'","'+body+'")'sql인젝션문제 
+
+#cursor.execute(sql)
+
+#connect.commit()
+#cursor.close()
+#connect.close()
+
+# print(title, body)  
+# import sqlite3
+# connect = sqlite3.connect('data.db')
+#with cursor = connect.cursor()
+
+#title = input('제목을 알려주세요: ')
+#body = input('본문을 알려주세요: ')
+#sql = 'insert into topics (title, body) values(?,?)'
+#cursor.execute(sql, (title, body))
+
+# sql = 'insert into topics (title, body) values("'+title+'","'+body+'")'sql인젝션문제 
+
+#cursor.execute(sql)
+
+#connect.commit()
+#cursor.close()
+#connect.close()
+
+# print(title, body)
